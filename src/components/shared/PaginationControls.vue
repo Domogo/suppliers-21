0db6f9a75e923956be4cce429caddf8f5980abd6
Domogo <domsatrapa@gmail.com>
2023-08-09 21:30:17 +0200

    chore: refactor to setup component

--- src/components/shared/PaginationControls.vue
+++ src/components/shared/PaginationControls.vue
@@ -1,42 +1,37 @@
-<script lang="ts">
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  emits: {
-    updateUrl(url: string) {
-      return typeof url === 'string'
-    }
+<script setup lang="ts">
+const props = defineProps({
+  url: {
+    type: String,
+    required: true
   },
-  props: {
-    url: {
-      type: String,
-      required: true
-    },
-    previous: {
-      type: String,
-      required: false
-    },
-    next: {
-      type: String,
-      required: false
-    },
-    count: {
-      type: Number,
-      required: false
-    }
+  previous: {
+    type: String,
+    required: false
+  },
+  next: {
+    type: String,
+    required: false
+  },
+  count: {
+    type: Number,
+    required: false
   }
+})
+
+const emit = defineEmits({
+  updateUrl: (url: string) => typeof url === 'string'
 })
 </script>
 
 <template>
   <div>
-    <button v-if="$props.previous" @click.prevent="$emit('updateUrl', $props.previous)">
+    <button v-if="props.previous" @click.prevent="emit('updateUrl', props.previous)">
       Previous
     </button>
-    <button v-if="$props.next" @click.prevent="$emit('updateUrl', $props.next)">Next</button>
+    <button v-if="props.next" @click.prevent="emit('updateUrl', props.next)">Next</button>
 
-    <div v-if="$props.count">
-      There are {{ $props.count }} <slot name="type">suppliers</slot> total.
+    <div v-if="props.count">
+      There are {{ props.count }} <slot name="type">suppliers</slot> total.
     </div>
   </div>
 </template>
